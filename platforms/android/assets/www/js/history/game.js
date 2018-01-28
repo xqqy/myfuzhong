9e74d51f1f1ac3d0c7ad6b045cf6a080cebe89ff
abcd <xqqy189@hotmail.com>
2018-01-28 23:53:57 +0800

    V2.0

---
+++ platforms/android/assets/www/js/history/game.js
@@ -0,0 +1,159 @@
+var story = new Array();
+story[0] = { //老校门
+	latitude: 39.8964,
+	//目标位置纬度
+	longitude: 116.3784,
+	//目标位置经度
+	info: '<p>南新华街的风起云涌<br>总少不了你的名字<br><span style="font-family:xk">师大附中</span><br><span style="float:right;font-size:20px">----这里是附中2017开篇语</span></p><p style="font-size:15px">玩法：根据文字走到对应的地点即可。</p>',
+	done: '<img src="../img/history/game1_1.jpg" alt="五城中学堂" height="100%" width="50%"><p>随着“中学概念”在中国的引进，设立中学堂的实践也在逐步展开。而设立师大附中的前身：五城中学堂的动议，也在戊戌变法之中被提出。 设立第一个国立中学——五城学堂的动议，是在戊戌变法时期。<br>御史张承缨奏请于五城添设小学堂、中学堂。军机处责成协办大学士、主管京师大学堂事务的孙家鼐“酌核办理”。并在当时上奏折建议建立中学校以确保京师大学堂生员。 在戊戌变法的阶段中，军机处在当日即批准建立。<br>可见，变法期间清政府的态度极其明确，即全力支持地方政府开办中学堂。同时也表明应“举贡生监”即从旧科举制度中西区人才。意在平衡旧科举制度和新大学制度。 而在戊戌变法失败之后，因和变法有着不可分开的关系的新学制建立也随之搁浅。在保守派执掌朝政的情况下新政大多被废除<br>之后庚子事变发生，结果之一是朝廷体制内的守旧顽固派被清除，因而在1901年清政府延续戊戌变法的思潮，启动了一场政治体制改革:新政，以求避免清朝灭亡。<br>于是，师大附中的前身：五城中学堂；就在内忧外患的中国建立了</p><h3>原诏书：</h3><p>会办五城事顺天府尹堂陈</p><p style="text-indent:24px">巡视五城察院</p><p style="text-indent:24px">为出示招考事照得</p><p style="text-indent:24px">光绪廿七年十月十五日内阁奉上谕，政务处奏请，节各省速办学堂等语。查袁世凯所奏山东学堂事宜及试办章程，拟先于省城立学堂一区，分斋督课。 先以备斋、正斋入手，俾初学易于速就。渐有师资，再行次第推广。 其教规、课程，参酌中西，而谆谆于明伦理，循礼法、尤得成德达才，本末兼资之道。著政务处即将该署督</p><br><p style="text-indent:24px">原奏，并单开章程，通行各省，立即仿照举办，毋许宕延等因。 钦此。复于十月廿五日内阁奉上谕，政务处会同礼部奏，遵旨核议学堂选举鼓励一折。 一折著照所请，节令各该省小学堂毕业学生考取功课合格者，选入中学堂肄业。 俟毕业后，考取合格者再选入该省大学堂。毕业后，取其合格者，给照作为优等学生， 由该督抚学政按其功课严密考校，拔优拟取，咨送京师大学堂复试候旨，钦定作为举人、贡生， 其贡生仍留下届应考，愿应乡试者，听俟举人积有成数，再由大学堂严加去取，咨送礼部， 奏请特派大臣考试。候旨钦定，作为进士，一体殿试，分别等第，带领引见，量加擢用。 不拘庶吉士、部属、中书等项成例，以励通才，而收实效。前据袁世凯奏，先于省城建立学堂， 分斋督课，其备斋、正斋，即寓小学堂、中学堂规制。业经谕令，各省仿照开办。所有此项学生， 著俟专斋毕业后，即照此所拟选举章程，一律办理，以示鼓励。钦此。仰见。庙谟宏远一以育才为先， 窃为学堂之设，原因学校旧制而变通之，名为创办，实则复古。方今时势多艰，需才孔亟、矧京师为首善之区， 尤当先行举办，为各省倡前。经本尹堂和本院会同具奏，就琉璃厂北后铁厂义塾旧址，开办五城学堂。九月廿日奏。</p><p style="text-indent:24px">前文总结自网络，诏书部分选自师大附中校档案第一卷</p><br><br><div id="buttom"></div>'
+}
+story[1] = { //赵世炎烈士像
+	latitude: 39.8967,
+	//目标位置纬度
+	longitude: 116.3791,
+	//目标位置经度
+	info: '<p><span style="font-family:xk">外争主权<br>内除国贼<br></span> <span style="float:right;font-size:20px">----五四运动口号</span></p><p style="font-size:15px">玩法：根据文字走到对应的地点即可。</p>',
+	done: '<img src="../img/history/game2_1.jpg" alt="师大附中学生参加1919年“五·四”运动。左一为赵士炎。" height="100%" width="50%"><p>赵世炎在1919年经李大钊介绍加入中国少年学会，积极参加五四爱国运动， 在“五四”运动中被师大附中学生推选为学生会干事长；组织和领导师大附中的同学走出校门， 同各大、中学校的师生一起参加爱国运动。</p><p>1920年5月，赵世炎赴法国勤工俭学，1921年春，与张申府、周恩来等发起成立旅法中国共产党早期组织， 成为中国共产党党员。之后与周恩来与周恩来等发起成立旅欧中国少年共产党，任中央执委会书记， 随后又任中共旅欧总支部委员和中共法国组书记。 1925年，发生五卅惨案，赵世炎领导和组织游行、罢工、罢课，声援上海人民的斗争 ，后组织和发动天津日商纱厂工人大罢工、开滦五矿工人大罢工、天津海员大罢工和赵各庄矿工人大罢工， 使北方地区的声援活动和群众斗争连成一片，沉重打击了帝国主义和反动军阀。</p><p>1927年7月2日晚，国民党警探根据叛变的省委秘书长的供词，赵世炎在自己的住所被捕。</p><h3>五四运动介绍</h3><p>五四运动发生于1919年（中华民国8年）5月4日的北洋政府治下的京兆地方， 为以青年学生为主的学生运动，以及包括广大公民、市民和工商人士等中基层广泛参与的一次示威游行、 请愿、罢课、罢工和暴力对抗政府等多形式的行动。事件起因在第一次世界大战完结后举行的巴黎和会中， 列强把德国在山东的权益转让给日本，即山东问题。当时北洋政府未能捍卫国家利益，有国人极度不满， 从而上街游行表达不满。当时最著名的口号之一是“外争国权（对抗列强侵权），内除国贼（惩除媚日官员）”。</p><p>五四运动使苏俄在中国播下无产阶级暴力革命的种子，中国共产党的成立和国民党的改组便充份显示出： 当时很多人认为“布尔什维克革命是当时半封建半殖民地中国的唯一出路”。但也有人认为当时对暴力革命的绝对化， 对中国的长远发展是一种破坏而非建设作用；有些人甚至认为被中国国民党或中国共产党统治，还不如成为殖民地。</p><p>1921年11月11日至1922年2月6日，美国、英国、日本、法国、意大利、荷兰、比利时、葡萄牙、 中国九国在美国首都华盛顿举行国际会议。中国政府时值颜惠庆内阁，派出施肇基、頋维钧、王宠惠三人为全权代表， 余日章、蒋梦麟为国民代表，朱佛定为华盛顿会议中国代表团秘书， 1922年2月4日，中国和日本还在华盛顿签订了《解决山东问题悬案条约》及其附约。条约规定， 日本将德国旧租借地交还中国，中国将该地全部开为商埠；原驻青岛、胶济铁路及其支线的日军应立即撤退； 青岛海关归还中国；胶济铁路及其支线归还中国等。附约中规定了对日本人和外国侨民的许多特殊权利， 但是中国通过该条约收回了山东半岛主权和胶济铁路权益。</p><p style="text-indent:24px">总结自维基百科、百度百科；图片部分选自师大附中校档案第一卷</p><br><br><div id="buttom"></div>',
+}
+story[2] = { //钱馆
+	latitude: 39.8958,
+	//目标位置纬度
+	longitude: 116.3785,
+	//目标位置经度
+	info: '<p><span style="font-family:xk;font-size:30px">我们也要搞人造卫星！<br>搞原子弹、氢弹、导弹</span> <span style="float:right;font-size:20px">----毛泽东</span></p><p style="font-size:15px">玩法：根据文字走到对应的地点即可。</p>',
+	done: '<h1>钱学森介绍：</h1><img src="../img/history/game3_1.jpg"alt="钱学森"height="100%"width="50%"/><h3>出国以前</h3>钱学森于1911年出生于上海，祖籍浙江省杭州市临安市。1923年9月，进入北京师范大学附属中学学习。1929年考入铁道部交通大学上海学校机械工程学院铁道工程系，1934年毕业于国立交通大学（现上海交通大学和西安交通大学），6月考取清华大学第七届庚款留美学生。<h3>赴美留学</h3>1935年9月进入美国麻省理工学院航空系学习<br/>1936年9月获麻省理工学院航空工程硕士学位，后转入加州理工学院航空系学习，成为世界著名的大科学家冯·卡门（Theodore von Kármán）的学生，并很快成为冯·卡门最重视的学生。他先后获航空工程硕士学位和航空、数学博士学位。<br/>1938年7月至1955年8月，钱学森在美国从事空气动力学、固体力学和火箭、导弹等领域研究，并与导师共同完成高速空气动力学问题研究课题和建立“卡门-钱学森”公式，在二十八岁时就成为世界知名的空气动力学家。<br/>1939年，获美国加州理工学院航空、数学博士学位。1943年，任加州理工学院助理教授。1945年，任加州理工学院副教授。<br/>1947年，任麻省理工学院教授。<br/>1947年，在上海与蒋英结婚。1949年，任加州理工学院喷气推进中心主任、教授。<br/>1953年，钱学森正式提出物理力学概念，主张从物质的微观规律确定其宏观力学特性，开拓了高温高压的新领域。<br/><h3>遭到拘留</h3>1949年当中华人民共和国宣告诞生的消息传到美国后，钱学森和夫人蒋英便商量着早日赶回祖国，为自己的国家效力。此时的美国，以麦卡锡为首对共产党人实行全面追查，并在全美国掀起了一股驱使雇员效忠美国政府的狂热。钱学森因被怀疑为共产党人和拒绝揭发朋友，被美国军事部门突然吊销了参加机密研究的证书。钱学森非常气愤，以此作为要求回国的理由。<br/>1950年，钱学森上港口准备回国时，被美国官员拦住，并将其关进监狱，而当时美国海军次长丹尼·金布尔（Dan A.Kimball）声称：钱学森无论走到哪里，都抵得上5个师的兵力。从此，钱学森在受到了美国政府迫害，同时也失去了宝贵的自由，他一个月内瘦了三十斤左右。移民局抄了他的家，在特米那岛上将他拘留14天，直到收到加州理工学院送去的1.5万美金巨额保释金后才释放了他。后来，海关又没收了他的行李，包括800公斤书籍和笔记本。美国检察官再次审查了他的所有材料后，才证明了他是无辜的。<h3>艰难归国</h3>50年代初期钱学森在美国受迫害的消息很快传到中国，中国科技界的朋友通过各种途径声援钱学森。党中央对钱学森在美国的处境极为关心，中国政府公开发表声明，谴责美国政府在违背本人意愿的情况下监禁了钱学森。<br/>1954年，一个偶然的机会，他在报纸上看到陈叔通站在天安门城楼上，身份是全国人大常委会副委员长，他决定给这位父亲的好朋友写信求救。正当周恩来总理为此非常着急的时候，时任全国人大常委会副委员长的陈叔通收到了一封从大洋彼岸辗转寄来的信。他拆开一看，署名“钱学森”，原来是请求祖国政府帮助他回国。<br/>1954年4月，美英中苏法五国在日内瓦召开讨论和解决朝鲜问题和恢复印度支那和平问题的国际会议。出席会议的中国代表团团长周恩来联想到中国有一批留学生和科学家被扣留在美国，于是就指示说，美国人既然请英国外交官与我们疏通关系，我们就应该抓住这个机会，开辟新的接触渠道。中国代表团秘书长王炳南1954年6月5日开始与美国代表、副国务卿约翰逊就两国侨民问题进行初步商谈。美方向中方提交了一份美国在华侨民和被中国拘禁的一些美国军事人员名单，要求中国给他们以回国的机会。为了表示中国的诚意，周恩来指示王炳南在1954年6月15日举行的中美第三次会谈中，大度地作出让步，同时也要求美国停止扣留钱学森等中国留美人员。<br/>然而，中方的正当要求被美方无理拒绝。1954年7月21日，日内瓦会议闭幕。为不使沟通渠道中断，周恩来指示王炳南与美方商定自1954年7月22日起，在日内瓦进行领事级会谈。为了进一步表示中国对中美会谈的诚意，中国释放了4个扣押的美国飞行员。<br/>中国作出的让步，最终是为了争取钱学森等留美科学家尽快回国，可是在这个关键问题上，美国代表约翰逊还是以中国拿不出钱学森要回国的真实理由，一点不松口。1955年，经过周恩来总理在与美国外交谈判上的不断努力——甚至包括了不惜释放11名在朝鲜战争中俘获的美军飞行员作为交换，1955年8月4日，钱学森收到了美国移民局允许他回国的通知。1955年9月17日，钱学森回国愿望终于得以实现了，这一天钱学森携带妻子蒋英和一双幼小的儿女，登上了“克利夫兰总统号”轮船，踏上返回祖国的旅途。1955年10月1日清晨，钱学森一家终于回到了自己魂牵梦绕的祖国，回到自己的故乡。<p style="text-indent:24px">总结自百度百科</p><br/><br/><br/><br/><br/><div id="buttom"></div>',
+}
+story[3] = { //礼堂
+	latitude: 39.8966,
+	//目标位置纬度
+	longitude: 116.3792,
+	//目标位置经度
+	info: '礼堂',
+	done: '<h1>附中现状</h1><img src="../img/history/game4_1.jpg"alt="附中全景图"height="100%"width="50%"/><p>目前，我校占地总面积22000平方米，总建筑面积21000平方米。操场占地6400平方米，200米跑道。学校图书馆藏书16万册，报刊310种。</p><p>学校现有高中24个班（含分校4个班），初中6个班，在校生1450人，外国留学生10人。全校现有教职工166人，其中任课教师111人，干部职工55人。任课教师中特级教师6人，高级教师56人；市区级学科带头人14人，市级青年骨干教师4人；教师中硕士研究生6人，研究生班结业30人。</p><p>北京师大附中建校98年来，历经风雨，精心办学，为国家培养了成千上万的优秀人才。师大附中在办学中不断探索、改革，积累了丰富的经验，在我国近现代教育发展的各个历史阶段，做出了自己的贡献。学校的发展受到了毛泽东、邓小平、江泽民三代党和国家领导人的关怀。1950年7月，毛泽东同志为我校亲笔书写校名；1987年邓小平同志为我校赵世炎烈士塑像题字；1996年10月，江泽民同志为我校书写了“育才兴国”的题词，这既是对我校的关怀与鼓励，更是赋予我校面向新世纪的重任。</p><p>附中悠久的历史，丰富的办学经验，饱含着我国近现代教育家的智慧和心血。在师大附中的历届领导、教师和学生的共同奋斗下，形成了爱国兴国、育人为本、严谨治学、师生平等、主动学习、全面发展的良好校风和光荣传统。随着社会的向前发展，我们要继续发扬附中在教育观念、管理体制、教育内容、教学方法等方面锐意改革、不断创新的精神，使师大附中逐步成为面向新世纪的基础教育改革基地，培养高素质人才的沃土。</p><h1>附中现代化历史</h1><p>1980年改建校舍。兴建教学大楼、实验楼、礼堂、食堂、大操场、学生宿舍楼、教师宿舍楼。</p><p>2001年1月12日国务院总理办公会决定：国家投资人民币3亿元对六所教育部在京直属高校附中（北京师大附中、二附中、实验中学、人大附中、北大附中、清华附中）进行扩招扩建，以满足广大人民群众对子女上好高中的愿望。</p><br><p>同年6月，综合教学楼结构建设基本完成。经市建委组织专家评估，初步通过长城杯评奖验收；我校礼堂设计参加北京市建筑设计研究院优秀设计评奖，经专家实地考察和评定，获一等奖。</p><p>2003年3月21日，和平门中学正式并入北京师大附中。并入后，师大附中校园占地面积达到66亩，学生达到72班3295人，教职工（含离退休人员）超过500人。</p><h1>附中礼堂大事记</h1><img src="../img/history/game4_2.jpg"alt="礼堂"height="100%"width="50%"/><p>2005年12月6日，著名地理特级教师王树声先生从教55周年教育教学研讨会在附中礼堂隆重举行。来自全国的同行欢聚一堂共同庆贺。</p><p>2008年4月30日，距离北京奥运会开幕还有100天之际，初一年级全体师生在学校礼堂隆重举行奥运100天倒计时牌揭幕仪式。</p><p>2010年9月10日，西城区教师节庆祝大会在全国政协礼堂隆重举行。我校地理教研组韩英英老师荣获首届“霍懋征奖”。</p><p style="text-indent:24px">总结自附中官网</p><br/><br/><br/><br/><br/><div id="buttom"></div>',
+}
+story[4] = {//小花园
+	latitude: 39.8961,
+	//目标位置纬度
+	longitude: 116.3793,
+	//目标位置经度
+	info: '小花园',
+	done: '也没写呢<p style="text-indent:24px">总结自附中官网</p><br/><br/><br/><br/><br/><div id="buttom"></div>',
+}
+
+
+//返回
+document.addEventListener("backbutton", onBackKeyDown, false);
+
+function back(index) {
+	if (index == 1) {
+		document.body.addEventListener("animationend", function() {
+			document.location = 'index.html';
+		})
+		document.body.style.animation = "hidden 0.3s forwards";
+	} else {
+		return;
+	}
+}
+
+function onBackKeyDown(e) {
+	e.preventDefault();
+	navigator.notification.confirm("", back, "您是指返回吗？", "是,否")
+}
+
+//地理服务
+
+function getPosition() {
+	var options = {
+		enableHighAccuracy: true,
+		timeout: 30000,
+		maximumage: 0
+	}
+	var watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
+
+	function onSuccess(position) {
+		console.log("位置信息：" + '\n' + '纬度: ' + position.coords.latitude + '\n' + '经度: ' + position.coords.longitude + '\n' + '获取时间戳: ' + position.timestamp);
+		document.getElementById("locate").innerHTML = ("位置信息：" + '\n' + '纬度: ' + position.coords.latitude + '\n' + '经度: ' + position.coords.longitude + '\n' + '获取时间戳: ' + position.timestamp);
+
+		if (story[localStorage.getItem("game")].latitude - 0.0001 < position.coords.latitude && story[localStorage.getItem("game")].latitude + 0.0001 > position.coords.latitude && story[localStorage.getItem("game")].longitude - 0.0001 < position.coords.longitude && story[localStorage.getItem("game")].longitude + 0.0001 > position.coords.longitude) {
+			navigator.geolocation.clearWatch(watchID);
+			if (localStorage.getItem("game") > localStorage.getItem("now") || !localStorage.getItem("now")) {
+				localStorage.setItem("now", localStorage.getItem("game"));
+			}
+			document.getElementById("info").style.animation = "hidden 0.7s forwards";
+			setTimeout(()=> {
+				document.getElementById("done").style.display = "block"
+				document.getElementById("done").style.animation = "doneen 2s forwards"
+				document.getElementById("buttom").innerHTML = "<button id='next'>完成</button>"
+			}, 0.2);
+			document.getElementById("locate").innerHTML = "";
+			document.getElementById("buttom").addEventListener("click", ()=> {
+				localStorage.setItem("game", parseInt(localStorage.getItem("game")) + 1);
+				document.body.style.animation = "hidden 0.5s forwards";
+				document.body.addEventListener("animationend", ()=> {
+					document.location = "game.html";
+				})
+			})
+		}
+		return;
+	};
+
+	function onError(error) {
+
+		console.log('code:' + error.code + '\n' + 'info:' + error.message)
+		switch (error.code) {
+		case 1:
+			dialogAlert("错误", "你必须开启位置服务才能使用本应用！应用将退出", "确定", navigator.app.exitApp);
+			break;
+		case 2:
+			dialogAlert("错误", "应用内部错误！提示信息:" + error.message, "确定", navigator.app.exitApp);
+			break;
+		case 3:
+			dialogAlert("错误", "获取地理位置超时！位置更新可能不准确", "确定");
+			break;
+		default:
+			dialogAlert("错误", '地理位置服务错误！代码: ' + error.code + '\n' + '默认错误帮助: ' + error.message + '\n', "确定", navigator.app.exitApp);
+		}
+		return;
+	}
+}
+
+//通知服务
+
+function dialogAlert(title = "错误", message, buttonname = "确定", callback = function() {
+	return;
+}) {
+	navigator.notification.alert(message, callback, title, buttonname);
+
+}
+
+
+var app = {
+	// Application Constructor
+	initialize: function() {
+		document.addEventListener('DeviceReady', this.ready.bind(this), false);
+		document.addEventListener('pause', this.pause.bind(this), false);
+	},
+
+	// deviceready Event Handler
+	//
+	// Bind any cordova events here. Common events are:
+	// 'pause', 'resume', etc.
+
+	// Update DOM on a Received Event
+	ready: function() {
+		console.log('cordova加载完成!');
+		if (!localStorage.getItem("game")) {
+			localStorage.setItem("game", "0");
+		}
+		if (localStorage.getItem("game") > 3) {
+			document.location = "done.html";
+		}
+		document.getElementById("info").innerHTML = story[localStorage.getItem("game")].info; //获取到游戏进度
+		document.getElementById("done").innerHTML = story[localStorage.getItem("game")].done;
+		document.body.style.animation = "showen 0.5s forwards"
+		getPosition();
+		return;
+	},
+
+};
+app.initialize();
